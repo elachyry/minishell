de40a2dcc1c45face86f15de32eba5b0ade612fc
Mohammed Elachyry <elachyrymohammed@gmail.com>
2024-03-29 04:31:54 +0000

    add all headers we will need, and create an empty main function

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,11 +6,23 @@
 /*   By: melachyr <melachyr@student.1337.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 03:57:48 by melachyr          #+#    #+#             */
-/*   Updated: 2024/03/29 03:58:23 by melachyr         ###   ########.fr       */
+/*   Updated: 2024/03/29 04:29:42 by melachyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <signal.h>
+# include <dirent.h>
+# include <errno.h>
+# include <termios.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 # endif
--- main.c
+++ main.c
@@ -6,7 +6,13 @@
 /*   By: melachyr <melachyr@student.1337.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 03:57:30 by melachyr          #+#    #+#             */
-/*   Updated: 2024/03/29 03:57:31 by melachyr         ###   ########.fr       */
+/*   Updated: 2024/03/29 04:29:18 by melachyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "includes/minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	return (0);
+}
