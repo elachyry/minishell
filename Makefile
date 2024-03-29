cefa8bb372dd4a6cb3d5cb9d33106535af1556c2
AmineKaddouri <kaddouriaminee@gmail.com>
2024-03-29 22:23:30 +0000

    create a prompet and display input

---
+++ Makefile
@@ -0,0 +1,24 @@
+NAME = minishell
+
+CC = cc
+
+CFLAGS = -Wall -Wextra -Werror
+
+SRCS = minishell.c
+
+OBJS = $(SRCS:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME) -lreadline
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re : fclean all
+
+.PHONY: all clean fclean re
---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaddouri <kaddouri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 01:49:28 by kaddouri          #+#    #+#             */
+/*   Updated: 2024/03/29 16:00:24 by kaddouri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+
+
+#endif
