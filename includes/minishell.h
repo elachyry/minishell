cb7e17e30322e4adb5f571d2e5ecc36ccfd92c73
Amine Kaddouri <akaddour@e1r1p11.1337.ma>
2024-05-07 08:41:57 +0100

    fix header file

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: akaddour <akaddour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 03:57:48 by melachyr          #+#    #+#             */
-/*   Updated: 2024/05/07 06:56:43 by akaddour         ###   ########.fr       */
+/*   Updated: 2024/05/07 08:41:10 by akaddour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,14 +88,10 @@ int		is_valid_key(char *key);
 //input_validation
 t_bool	has_unclosed_parenthesis(char *input);
 t_bool	has_unclosed_quotes(char *input);
-t_bool	has_invalid_redirections(const char *input);
-t_bool	has_misplaced_operators(const char *input);
+t_bool	has_invalid_redirections(char *input);
+t_bool	has_misplaced_operators(char *input);
 t_bool	syntax_checker(char	*input);
 t_bool	syntax_error_checker(char	*input);
-
-int	is_invalid_operator(const char **input);
-const char	*skip_spaces(const char *input);
-void	update_quote_counts(char c, int *s_q_count, int *d_q_count);
 
 //execution
 void	execution(void);
