2f317bdbb332aa7089e45a49f529187f565257d8
Mohammed Elachyry <melachyr@student.42.fr>
2024-05-26 15:59:28 +0100

    here_doc

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akaddour <akaddour@student.42.fr>          +#+  +:+       +#+        */
+/*   By: melachyr <melachyr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 03:57:48 by melachyr          #+#    #+#             */
-/*   Updated: 2024/05/24 19:11:20 by akaddour         ###   ########.fr       */
+/*   Updated: 2024/05/26 15:57:30 by melachyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@
 # include "../libraries/libft/libft.h"
 # include "../libraries/get_next_line/get_next_line.h"
 # define READLINE_LIBRARY
-# include "/Users/akaddour/readline/include/readline/readline.h"
-# include "/Users/akaddour/readline/include/readline/history.h"
-// # include "/Users/melachyr/readline/readline.h"
-// # include "/Users/melachyr/readline/history.h"
+// # include "/Users/akaddour/readline/include/readline/readline.h"
+// # include "/Users/akaddour/readline/include/readline/history.h"
+# include "/Users/melachyr/readline/readline.h"
+# include "/Users/melachyr/readline/history.h"
 
 typedef struct s_env
 {
